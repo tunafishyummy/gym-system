e5d1ce67d2fc3b2efc267b6c4b7daff1a84cecd7
tunafishyum <adinoellamio@gmail.com>
2025-11-06 12:26:01 +0800

    initial commit

--- gyms1.cpp
+++ gyms1.cpp
@@ -7,7 +7,7 @@
 #include <ctime>
 #include "libraryqrcode/qrcodegen.hpp"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "stb_image_write.h" //goo goo gaga
 
 
 using namespace std;
